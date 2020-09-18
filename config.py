7fe02855fa29765b0568aff58054610c957b5b5e
Ростичег Ростичег <rosticheg@gmail.com>
2020-09-18 12:37:00 +0300

    add missing air quality url

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ url = 'http://api.openweathermap.org/data/2.5/weather'
 
 #Air quality
 AIR_KEY = ''
-air_url = ''
+air_url = 'https://api.waqi.info/feed/kiev/?token='
            
 #TelegramBot token
 token = ''
